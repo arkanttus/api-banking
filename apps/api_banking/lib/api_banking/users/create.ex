59b0a5195a3c01e689e71d42d9bfee6bc8bd45cb
Italo Oliveira <italo.oliveira300@hotmail.com>
2021-04-15 13:42:17 -0500

    feat: create User

---
+++ apps/api_banking/lib/api_banking/users/create.ex
@@ -0,0 +1,39 @@
+defmodule ApiBanking.Users.Create do
+  alias ApiBanking.Repo
+  alias ApiBanking.Users.Schemas.User
+
+  require Logger
+
+  def create_user(struct) when is_struct(struct) do
+    params = %{email: struct.email, name: struct.name, password_hash: struct.password_hash}
+    create_user(params)
+  end
+
+  def create_user(params) do
+    Logger.info("Inserting new User")
+
+    params
+    |> User.changeset()
+    |> Repo.insert()
+
+    with %{valid?: true} = changeset <- User.changeset(params),
+         {:ok, user} <- Repo.insert(changeset) do
+      {:ok, user}
+    else
+      %{valid?: false} = changeset ->
+        msg_error =
+          Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+            Enum.reduce(opts, msg, fn {key, value}, acc ->
+              String.replace(acc, "%{#{key}}", to_string(value))
+            end)
+          end)
+
+        {:error, %{msg_error: msg_error}}
+    end
+  rescue
+    Ecto.ConstraintError ->
+      Logger.error("Email already in use")
+
+      {:error, :email_conflict}
+  end
+end
